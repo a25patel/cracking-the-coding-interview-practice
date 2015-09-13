4fc16d9ae1a3027c42a7088518176afa97d69604
a25patel <aanaletap2510@gmail.com>
2015-09-13 16:57:56 -0600

    working on romanNumeral reveral

--- other/romanNumerals.js
+++ other/romanNumerals.js
@@ -1,9 +1,6 @@
 // * Write a function that accepts a roman Number and returns an
 // integer.
-//
-// example: Input VII   Output 7
-//          Input XIX   Output 19
-//
+
 // Rule of Roman Numerals:
 // 1. A letter repeated, repeats its value many times
 // 2. Letters placed after another letter of greater value increases the value by the amounts of the smaller values
@@ -14,49 +11,70 @@
 // 4. A bar over the top of the latter or string of letters multiplies the number by 1,000
 
 // STRETCH:::: FLIP IT!!! Take an integer and output the roman numeral!!!
-// QUIZ:::: Do it recursively, email code and rationale!
 
 
-module.exports = function roman(string){
-  var numerals = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000,
-  };
+// module.exports = function roman(string){
 
-  function convert(string){
-    string = string.toUpperCase().split('');
-    var numbers = [];
-    for (var i = 0; i < string.length; i++) {
-      numbers.push(numerals[string[i]]);
-    }
-    var i = 0;
-    var total = 0;
-    addThem(numbers, i, total);
-  },
+// };
 
-  function addThem(array, i, total){
-    if( i === array.length){
-      console.log(total);
-      return total;
-    }else {
-      operator = (array[i] < array[i + 1]) ? -1 : 1;
-      total += operator * array[i];
-      addThem(array, ++i, total);
-    }
-  }
-
+var numerals = {
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  'C': 100,
+  'D': 500,
+  'M': 1000,
 };
 
-// convert('xix'); //19
-// convert('lvi'); //56
-// convert('cmlxiii'); //963
-// convert('cclxxxi'); //281
+function convert(string){
+  string = string.toUpperCase().split('');
+  var numbers = [];
+  for (var i = 0; i < string.length; i++) {
+    numbers.push(numerals[string[i]]);
+  }
+  var i = 0;
+  var total = 0;
+  addThem(numbers, i, total);
+}
 
+function addThem(array, i, total){
+  if( i === array.length){
+    console.log(total);
+    return total;
+  }else {
+    operator = (array[i] < array[i + 1]) ? -1 : 1;
+    total += operator * array[i];
+    addThem(array, ++i, total);
+  }
+}
+
+
+convert('xix'); //19
+convert('lvi'); //56
+convert('cmlxiii'); //963
+convert('cclxxxi'); //281
+
+
+
+To reverse this:
+
+1. find out how many places.
+2. find the base of that place and multiply by the number
+  ex: 42: 10 is base so multiply by 4.
+  ex: 863: 500 is base so subtract 5 from 8 then find base (100) and multiply by 3!
+move on to the next number/place and do the same.
+3. for subtractions :
+  if number is one less than a base case (usually a 4 or 9!)
+  then write the higher base case and subtract one!
+
+!!!!!!!!!
+first check if its a 4 or 9!
+if it is
+  find the base case that is higher, and subtract one
+else
+  find the base case that is lower and multiply but number
+  
 
 
 // ORIGINAL FUNCTION
@@ -70,20 +88,6 @@ module.exports = function roman(string){
 //     }
 //   }
 //   console.log(total);
-// }
-
-// // RECURSIVE FUNCTION
-// function addThem(array, i, grand_total){
-//   if( i === array.length){
-//     console.log(grand_total);
-//     return grand_total;
-//   }else if(array[i] < array[i + 1]){
-//     grand_total += -array[i];
-//     addThem(array, ++i, grand_total);
-//   }else{
-//     grand_total += array[i];
-//     addThem(array, ++i, grand_total);
-//   }
 // }
 
 
